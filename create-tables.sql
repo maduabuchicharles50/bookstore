f45c65fa6ddb9b22d856d9683e9152e604189fa4
MbonuCharles <maduabuchicharles50@gmail.com>
2024-07-02 23:55:41 +0100

    Feat:added docker and database

---
+++ create-tables.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+CREATE TABLE books (
+    id serial not null primary key,
+    author_id integer not null references authors (id),
+    genre_id integer not null references genres (id),
+    title text not null,
+    author text not null,
+    isbn varchar(255) default null,
+    publication_year varchar(50) default null,
+    created_at timestamp not null default now(),
+    updated_at timestamp null default now()
+);
+
+CREATE TABLE authors (
+    id serial not null primary key,
+    name text not null,
+    contact varchar(50) default null,
+    created_at timestamp not null default now(),
+    updated_at timestamp null default now()
+);
+
+CREATE TABLE genres (
+    id serial not null primary key,
+    type text not null
+);
+
+COMMIT;
